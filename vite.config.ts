5ce65a0738ff9d69784ac36d2a16a81d9b6a5621
leon123858 <a0970785699@gmail.com>
2024-02-19 01:54:24 +0800

    git page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/my-class-schedule/',
     plugins: [react(), VitePWA({
         registerType: 'autoUpdate', devOptions: {
             enabled: true
