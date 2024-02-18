53ee732b1af07e0106fccac0bc6dd39b6b209ca8
leon123858 <a0970785699@gmail.com>
2024-02-19 01:51:36 +0800

    first class OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,86 @@
 import {Schedule} from "./components/Schedule.tsx";
+import {ClassUnit} from "./utils/interpreter.ts";
 
+const data: ClassUnit[] = [
+    {
+        name: '分散式系統',
+        place: '資105',
+        time: [
+            {
+                day: 1,
+                time: 6,
+            },
+            {
+                day: 1,
+                time: 7,
+            },
+            {
+                day: 1,
+                time: 8,
+            },
+        ],
+    },
+    {
+        name: '演算法的數學解析',
+        place: '資107',
+        time: [
+            {
+                day: 2,
+                time: 3,
+            },
+            {
+                day: 2,
+                time: 4,
+            },
+            {
+                day: 5,
+                time: 4,
+            }
+        ],
+    },
+    {
+        name: "系統軟體設計與實作特論",
+        place: "資111",
+        time: [
+            {
+                day: 3,
+                time: 7,
+            },
+            {
+                day: 3,
+                time: 8,
+            },
+            {
+                day: 3,
+                time: 9,
+            }
+        ],
+    },
+    {
+        name: "業界視角下的資訊安全",
+        place: "資105",
+        time: [
+            {
+                day: 5,
+                time: 7,
+            },
+            {
+                day: 5,
+                time: 8,
+            },
+        ],
+    }
+]
 function App() {
 
     return (
-        <>
-            <Schedule/>
-        </>
+        <div style={{
+            padding: '20px',
+            maxWidth: '800px',
+            margin: 'auto',
+        }}>
+            <Schedule data={data}/>
+        </div>
     )
 }
 
--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -4,28 +4,9 @@ import {useEffect, useState} from "react";
 import {ClassUnit, interpret} from "../utils/interpreter.ts";
 import {Day} from "./Day.tsx";
 
-const data: ClassUnit[] = [
-    {
-        name: '分散式系統',
-        place: '資105',
-        time: [
-            {
-                day: 1,
-                time: 6,
-            },
-            {
-                day: 1,
-                time: 7,
-            },
-            {
-                day: 1,
-                time: 8,
-            },
-        ],
-    }
-]
-
-export const Schedule = () => {
+export const Schedule = ({data}: {
+    data: ClassUnit[];
+}) => {
     const [activeKey, setActiveKey] = useState('1');
     const items: TabsProps['items'] = [
         {
@@ -36,27 +17,22 @@ export const Schedule = () => {
         {
             key: '2',
             label: '週二',
-            children: 'Content of Tab Pane 2',
+            children: <Day items={interpret(data, 2)}/>,
         },
         {
             key: '3',
             label: '週三',
-            children: 'Content of Tab Pane 3',
+            children: <Day items={interpret(data, 3)}/>,
         },
         {
             key: '4',
             label: '週四',
-            children: 'Content of Tab Pane 3',
+            children: <Day items={interpret(data, 4)}/>,
         },
         {
             key: '5',
             label: '週五',
-            children: 'Content of Tab Pane 3',
-        },
-        {
-            key: '6',
-            label: '週末',
-            children: 'Content of Tab Pane 3',
+            children: <Day items={interpret(data, 5)}/>,
         },
     ];
     useEffect(() => {
@@ -64,9 +40,11 @@ export const Schedule = () => {
         const today = new Date().getDay();
         // 週一到週五的 index 分別是 1 到 5
         if (today === 0 || today === 6) {
-            setActiveKey('6');
+            setActiveKey('1');
         }
         setActiveKey(String(today));
     }, []);
-    return <Tabs activeKey={activeKey} items={items}/>;
+    return <Tabs activeKey={activeKey} items={items} onChange={(e)=>{
+        setActiveKey(e);
+    }}/>;
 };
